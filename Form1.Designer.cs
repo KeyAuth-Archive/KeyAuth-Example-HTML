776a391c186eeefe430598352d5dd53bac063bae
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2021-12-30 02:23:24 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace KeyAuthExample
+{
+    partial class LoaderInit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.ActivateBrowserOnCreation = false;
+// TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(433, 393);
+            this.browser.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 393);
+            this.Controls.Add(this.browser);
+            this.Name = "Form1";
+            this.Text = "html embedded in winforms - keyauth example";
+            this.Load += new System.EventHandler(this.Init);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser browser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using CefSharp;
+using Keyauth;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyAuthExample
+{
+    public partial class LoaderInit : Form
+    {
+        public LoaderInit() => InitializeComponent();
+
+        static string name = "Citrus";
+        static string ownerid = "7pr3DBb5Id";
+        static string secret = "12e93942aeed7be8bcaf9f74709612ba54b5230f4f5951a85ef77b0c34d71737";
+        static string version = "1.0";
+
+        public static api KeyAuthApp = new api(name, ownerid, secret, version);
+        public class UserInformation
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+            public string key { get; set; }
+        }
+        public void HandleInformation(string info)
+        {
+            UserInformation json = JsonConvert.DeserializeObject<UserInformation>(info);
+
+            string username = json.username;
+            string password = json.password;
+            string key = json.key;
+
+            if (json.key != null) { Register(username, password, key); }
+            else { Login(username, password); }
+        }
+        public void Login(string username, string password) 
+        { 
+            if (KeyAuthApp.login(username, password))
+            { 
+                //Optional Success Message / Form Open
+                MessageBox.Show($"Welcome back, {KeyAuthApp.user_data.username}!");
+            } 
+        }
+        public void Register(string username, string password, string key) 
+        { 
+            if (KeyAuthApp.register(username, password, key)) 
+            { 
+                MessageBox.Show($"Thanks for registering, {KeyAuthApp.user_data.username}!"); 
+            } 
+        }
+        private void Init(object sender, EventArgs e) 
+        { 
+            browser.JavascriptMessageReceived += MessageRecieved; 
+            KeyAuthApp.init(); browser.LoadHtml(Properties.Resources.index); 
+        }
+        private void MessageRecieved(object sender, JavascriptMessageReceivedEventArgs e) => 
+        new Thread(() => { Thread.CurrentThread.IsBackground = true; HandleInformation((string)e.Message.ToString()); }).Start();
+    }
+}
